fc5eb4822ef1a52fbfb2d3002a440e466ce41228
alibama <adp6j@virginia.edu>
2023-08-21 17:36:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,28 @@ import json
 
 st.title('Parse Bank Transaction Data')
 
-uploaded_file = st.file_uploader('Upload JSON file', type=['json'])
+import json
 
-data = json.loads(uploaded_file.getvalue())
+uploaded_file = st.file_uploader(...) 
 
+if uploaded_file:
 
-data = [data]
+  data = json.loads(uploaded_file.getvalue())
 
+  if type(data) == dict:
+     data = [data]
 
-collateral = []
-spend = []
+  collateral = []
+  spend = []
 
-for row in data:
-  if 'collateral_add' in row['type']:
-      collateral.append(row)  
-  elif 'spend' in row['type']:
+  for row in data:
+    if 'collateral_add' in row['type']:
+      collateral.append(row)
+    elif 'spend' in row['type']:
       spend.append(row)
 
-# Create DataFrames after parsing JSON
-collateral_df = pd.DataFrame(collateral)
-spend_df = pd.DataFrame(spend)
+  print(f"Collateral rows: {len(collateral)}") 
+  print(f"Spend rows: {len(spend)}")
 
-
-st.write('Collateral Transactions')
-st.write(collateral_df.head())
-
-st.write('Spend Transactions')
-st.write(spend_df.head())
+  collateral_df = pd.DataFrame(collateral)
+  spend_df = pd.DataFrame(spend)
