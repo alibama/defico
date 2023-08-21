1bac61b8924bbc3225365178a4707264b305c26b
alibama <adp6j@virginia.edu>
2023-08-21 17:22:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,21 @@ import json
 
 st.title('Parse Bank Transaction Data')
 
-uploaded_file = st.file_uploader('Upload JSON file', type=['json'])
 if uploaded_file is not None:
-    data = json.loads(uploaded_file.getvalue())
-  
-collateral = []
-spend = []
 
-if type(data) == dict:
-    data = [data]
+  data = json.loads(uploaded_file.getvalue())
   
-for row in data:
-    if 'collateral_add' in row['type']: 
-        collateral.append(row)
-    elif 'spend' in row['type']:
-        spend.append(row)
+  if type(data) == dict:
+    data = [data]
+
+  collateral = []
+  spend = []
+
+  for row in data:
+    if 'collateral_add' in row['type']:
+      collateral.append(row)
+    elif 'spend' in row['type']:  
+      spend.append(row)
 
 collateral_df = pd.DataFrame(collateral) 
 spend_df = pd.DataFrame(spend)
