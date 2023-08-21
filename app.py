5007b51691c9610d3f8f9d63d29e398383024b4e
alibama <adp6j@virginia.edu>
2023-08-21 15:54:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,29 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 import json
-import plotly.express as px
 
 st.title('JSON Transaction Explorer')
 
 uploaded_file = st.file_uploader('Choose a JSON file', type='json')
 
 if uploaded_file is not None:
-    data = json.load(uploaded_file)
+
+  data = json.load(uploaded_file)
+
+  # Schema detection
+  transactions = data['transactions']
+  schemas = {}
+
+  for transaction in transactions:
+
+    t_type = transaction['type']
+
+    if t_type not in schemas:
+      schemas[t_type] = {}
     
-    df = pd.json_normalize(data)
+    schemas[t_type] = transaction.keys()
 
-    # Display full table
-    st.dataframe(df)  
-
-    # Analysis
-    totals = df['amount'].describe()
-    st.write("Total Transactions:", len(df))
-
-    st.write("Total Amount:")
-    st.metric(label="Sum", value=totals['sum'])
-    st.metric(label="Average", value=totals['mean'])
-    st.metric(label="Min", value=totals['min'])
-    st.metric(label="Max", value=totals['max'])
-
-    # Visualizations 
-    fig = px.histogram(df, x="amount")
-    st.plotly_chart(fig)
-
-    fig = px.pie(df, values='amount', names='merchantName')
-    st.plotly_chart(fig)
+  # Print schemas
+  st.header("Detected Schemas")
+  for t_type, schema in schemas.items():
+    st.write(f"{t_type} schema: {sorted(schema)}")
